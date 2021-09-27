2249135e4852c5eea0467a23d14a887ac6cce5e7
Gayathri Buddhika <39160904+gayathribuddhika@users.noreply.github.com>
2021-09-27 22:45:51 +0530

    create react app, install bootstrap

---
+++ crudreact/src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="container">
+        <h2>React CRUD Operations</h2>
+      </div>
+    )
+  }
+}
+
+
+export default App;
